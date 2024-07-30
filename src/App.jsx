94a995692322fbe5a84bb0f6e7b7017df72db6dc
Narayan Reddy <baddam.narayan@gmail.com>
2024-07-30 15:43:08 -0400

    added context and useReducer

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 import Header from "./assets/components/Header";
 import Meals from "./assets/components/Meals";
+import { CartContextProvider } from "./store/CartContext";
 
 function App() {
   return (
-    <>
+    <CartContextProvider>
       <Header />
       <Meals />
-    </>
+    </CartContextProvider>
   );
 }
 
--- src/assets/components/Header.jsx
+++ src/assets/components/Header.jsx
@@ -1,6 +1,12 @@
+import { useContext } from "react";
 import logo from "../logo.jpg";
 import Button from "./UI/Button";
+import CartContext from "../../store/CartContext";
 export default function Header() {
+  const cartCtx = useContext(CartContext);
+  const totalcartItems = cartCtx.items.reduce((total, item) => {
+    return total + item.quantity;
+  }, 0);
   return (
     <header id="main-header">
       <div id="title">
@@ -8,7 +14,7 @@ export default function Header() {
         <h1> ReactFood</h1>
       </div>
       <nav>
-        <Button textOnly>Cart (0)</Button>
+        <Button textOnly>Cart ({totalcartItems})</Button>
       </nav>
     </header>
   );
